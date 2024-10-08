6699dbd51661716f3f7301b833086410bc31d6b1
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-10-08 22:00:28 +0530

    fully changed the index page , added user login page , admin page added

---
+++ admin/static/js/custom.js
@@ -0,0 +1,40 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+//  owl carousel script
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+//    end owl carousel script 
+
+
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
