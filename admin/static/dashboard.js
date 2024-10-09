d06657d1d321d6a9c6fb1c87f69e61e0f53d0a6a
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-10-09 15:42:01 +0530

    dashboard page added with fevicon and ui enhaced in index page

--- admin/static/dashboard.js
+++ admin/static/dashboard.js
@@ -13,6 +13,18 @@ let menu = document.querySelector(".menu")
 let sidebar = document.querySelector(".sidebar")
 let mainContainer = document.querySelector(".main--container")
 
+let help = document.querySelector(".help")
+
+help.onclick() = function(){
+    help.classList.toggle("active");
+    
+}
+let ic = document.querySelector("#ic")
+
+help.onclick() = function(){
+    ic.classList.toggle("active");
+    
+}
 
 menu.onclick = function(){
     sidebar.classList.toggle("activemenu")
