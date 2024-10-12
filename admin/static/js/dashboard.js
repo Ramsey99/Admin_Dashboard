a93cf4b4267abdb36044d89cae4549a21b91d500
ak0404 <anushakhurana48@gmail.com>
2024-10-12 10:54:24 +0530

    Add dark mode toggle functionality and maintain sidebar features

--- admin/static/js/dashboard.js
+++ admin/static/js/dashboard.js
@@ -1,22 +1,24 @@
-let body = document.querySelector(".body")
-let sun = document.querySelector(".sun")
-let moon = document.querySelector(".moon")
+let body = document.querySelector(".body");
+let sun = document.querySelector(".sun");
+let moon = document.querySelector(".moon");
 
-moon.onclick = function(){
-    body.classList.add("dark--mode")
-}
-sun.onclick = function(){
-    body.classList.remove("dark--mode")
+// Toggle dark mode
+function toggleDarkMode() {
+    body.classList.toggle("dark--mode");
 }
 
-let menu = document.querySelector(".menu")
-let sidebar = document.querySelector(".sidebar")
-let mainContainer = document.querySelector(".main--container")
+moon.onclick = toggleDarkMode;
+sun.onclick = toggleDarkMode;
 
+// Sidebar functionality
+let menu = document.querySelector(".menu");
+let sidebar = document.querySelector(".sidebar");
+let mainContainer = document.querySelector(".main");
 
-menu.onclick = function(){
-    sidebar.classList.toggle("activemenu")
-}
-mainContainer.onclick = function(){
-    sidebar.classList.remove("activemenu")
-}
+menu.onclick = function() {
+    sidebar.classList.toggle("activemenu");
+};
+
+mainContainer.onclick = function() {
+    sidebar.classList.remove("activemenu");
+};
